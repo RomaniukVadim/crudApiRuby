5c68f62e8504e681d8293aded3dd1d61f0f32413
Meryborn <lavadimka1997@gmail.com>
2016-07-03 03:01:46 +0300

    :beer: CRUD - Read - Find Method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		
+		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
 		
 	end
 
-	def destroy 
-
+	def destroy
+		
 	end
 end
